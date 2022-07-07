90e6a02e45d30e70b66738e2f5c4706fe242d1d9
Yasnovskiy <andrey5.1997@mail.ru>
2022-07-07 18:27:45 +0500

    chore: #2hcybgy: translating the Template component to TypeScript

--- src/template/Template.tsx
+++ src/template/Template.tsx
@@ -19,17 +19,18 @@ import { useSidebarRoutes } from './hooks/useSidebarRoutes';
 
 import { adminRoutes, sidebarRoutes } from '../routes/adminRoutes';
 
-export default function Template() {
+function Template() {
   const location = useLocation();
 
   const parsedSidebarRoutes = useSidebarRoutes(sidebarRoutes, location);
+
   const breadcrumbs = useBreadcrumbs(adminRoutes, { excludePaths: ['/'] });
 
   const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(false);
   const [isMobileSidebarOpened, setIsMobileSidebarOpened] = useState(false);
 
   const prevBreadcrumbPath = breadcrumbs.length > 1
-    ? breadcrumbs[breadcrumbs.length - 2].breadcrumb.key
+    ? breadcrumbs[breadcrumbs.length - 2].key
     : null;
 
   return (
@@ -48,7 +49,7 @@ export default function Template() {
             onCollapseToggle={() => setIsSidebarCollapsed(!isSidebarCollapsed)}
             onOverlayClick={() => setIsMobileSidebarOpened(false)}
             onMenuLinkClick={() => setIsMobileSidebarOpened(false)}
-            renderBottomComponent={(props) => (
+            renderBottomComponent={(props : { portalTarget: HTMLDivElement | null }) => (
               <>
                 <SidebarSettingsControl portalTarget={props.portalTarget} />
 
@@ -59,6 +60,7 @@ export default function Template() {
                 />
               </>
             )}
+            renderTopComponent={undefined}
           />
         </div>
 
@@ -85,3 +87,5 @@ export default function Template() {
     </>
   );
 }
+
+export default Template;
